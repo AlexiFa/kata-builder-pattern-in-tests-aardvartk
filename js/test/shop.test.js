e3ca169fef3aae17016f1bd2cf3e93e7dbd9d197
Alexis Faure <113013323+AlexiFa@users.noreply.github.com>
2024-11-27 10:20:26 +0100

    add builder design pattern

--- js/test/shop.test.js
+++ js/test/shop.test.js
@@ -8,7 +8,12 @@ const fsfAddress = new Address("51 Franklin Street", "Fifth Floor", "Boston", "0
 const parisAddress = new Address("33 quai d'Orsay", "", "Paris", "75007", "France")
 
 test('happy path', t => {
-    const user = new User({ name: "Bob", "email": "bob@domain.tld", age: 25, address: fsfAddress, verified: true })
+    const user = new User("Bob")
+        .setAge(25)
+        .setVerified(true)
+        .setAddress(fsfAddress)
+        .build()
+    // const user = new User({ name: "Bob", "email": "bob@domain.tld", age: 25, address: fsfAddress, verified: true })
 
     t.true(Shop.canOrder(user))
     t.false(Shop.mustPayForeignFee(user))
@@ -16,21 +21,36 @@ test('happy path', t => {
 })
 
 test('minor users cannot order from the shop', t => {
-    const user = new User({ name: "Bob", "email": "bob@domain.tld", age: 16, address: fsfAddress, verified: true })
+    const user = new User("Bob")
+        .setAge(16)
+        .setVerified(true)
+        .setAddress(fsfAddress)
+        .build()
+    // const user = new User({ name: "Bob", "email": "bob@domain.tld", age: 16, address: fsfAddress, verified: true })
 
     t.false(Shop.canOrder(user))
     t.end()
 })
 
 test('must be a verified user to order from the shop', t => {
-    const user = new User({ name: "Bob", "email": "bob@domain.tld", age: 20, address: fsfAddress, verified: false })
+    const user = new User("Bob")
+        .setAge(20)
+        .setVerified(false)
+        .setAddress(fsfAddress)
+        .build()
+    // const user = new User({ name: "Bob", "email": "bob@domain.tld", age: 20, address: fsfAddress, verified: false })
 
     t.false(Shop.canOrder(user))
     t.end()
 })
 
 test('foreigners must pay foreign fee', t => {
-    const user = new User({ name: "Bob", "email": "bob@domain.tld", age: 25, address: parisAddress, verified: true })
+    const user = new User("Bob")
+        .setAge(25)
+        .setVerified(true)
+        .setAddress(parisAddress)
+        .build()
+    // const user = new User({ name: "Bob", "email": "bob@domain.tld", age: 25, address: parisAddress, verified: true })
 
     t.true(Shop.mustPayForeignFee(user))
     t.end()
--- js/user.js
+++ js/user.js
@@ -1,9 +1,31 @@
 class User {
-    constructor({ name, age, verified, address }) {
+    constructor(name) {
         this.name = name
+        this.age = null
+        this.verified = null
+        this.address = null
+    }
+
+    setAge(age) {
         this.age = age
+        return this
+    }
+
+    setVerified(verified) {
         this.verified = verified
+        return this
+    }
+
+    setAddress(address) {
         this.address = address
+        return this
+    }
+
+    build() {
+        if (this.age === null) {
+            throw new Error("age is required")
+        }
+        return this
     }
 }
 
